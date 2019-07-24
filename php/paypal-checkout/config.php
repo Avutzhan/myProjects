feb5c5fca45f5fd3b8759bca684d1e787341f849
Avutzhan Dautov <dautov92@list.ru>
2019-07-24 21:42:11 +0600

    paypal

---
+++ php/paypal-checkout/config.php
@@ -0,0 +1,6 @@
+<?php
+$config ['authtoken'] = 'AbWzguLqGt39WOh1qRIW4lw-MyhDgdnsUCTUnjtQ3iExJJa6dthdD7MgMRAtmV6A-4N8p3Fbdlnw1VSC';
+$config ['posturl'] = 'https://www.sandbox.paypal.com/cgi-bin/webscr';
+$config ['business'] = 'dautov92@list.ru';
+$config ['returnurl'] = 'http://localhost/success.php';
+?>
